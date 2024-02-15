b27cdc79d7c1dee9e7fe97ab34d57170218dec0c
Waleed Lugod <lugodwaleed@gmail.con>
2024-02-15 12:24:26 +0800

    Created recipe ingredients pages

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import recipe_list
+from .views import recipe_list, recipe1, recipe2
 
 urlpatterns = [
     path("recipes/list", recipe_list, name="recipe_list"),
+    path("recipe/1", recipe1, name="recipe1"),
+    path("recipe/2", recipe2, name="recipe2"),
 ]
 
 app_name = "ledger"
