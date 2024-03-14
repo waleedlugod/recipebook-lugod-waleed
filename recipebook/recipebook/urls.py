b7566014b33479d1d0887fd8113d89cc00615ef6
Waleed Lugod <lugodwaleed@gmail.com>
2024-03-14 19:45:02 +0800

    Created login logout flow

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("ledger.urls", namespace="ledger")),
     path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
