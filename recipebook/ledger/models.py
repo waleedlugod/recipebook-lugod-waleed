1fd0ac997aac03d74d27a2f1d859b60090583d57
Waleed Lugod <lugodwaleed@gmail.com>
2024-03-14 16:05:49 +0800

    Created Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    biography = models.TextField(
+        # bio more than 255 characters
+        validators=[MinLengthValidator(256)]
+    )
 
 
 class Ingredient(models.Model):
